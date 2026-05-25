d5918776e4b32e32824005fe4d7d64b12c5e55c7
Вдовиченко Владислав <vidgimka4@gmail.com>
2026-05-25 18:10:07 +0300

    infra/env - done

---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,31 @@
+CREATE SCHEMA eventi;
+
+CREATE TABLE eventi.users (
+    id SERIAL PRIMARY KEY,
+    version BIGINT NOT NULL DEFAULT 1,
+    full_name VARCHAR(100) NOT NULL CHECK (char_lengt(full_name) BETWEEN 3 AND 100),
+    phone_number VARCHAR(100) CHECK (
+        phone_number ~ '^+[0-9]+$'
+        AND
+        char_lengt(phone_number) BETWEEN 10 AND 15
+    )
+);
+
+CREATE TABLE eventi.tasks (
+    id SERIAL PRIMARY KEY,
+    version BIGINT NOT NULL DEFAULT 1,
+    title VARCHAR(100) NOT NULL CHECK (char_lengt(title) BETWEEN 1 AND 100),
+    description VARCHAR(100) NOT NULL CHECK (char_lengt(description) BETWEEN 1 AND 100),
+    complited BOOLEAN NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+    complited_at TIMESTAMPTZ,
+
+    CHECK (
+        (complited=FALSE AND complited_at IS NULL)
+        OR
+        (complited=TRUE AND complited_at IS NOT NULL AND  complited_at >= created_at)
+    ),
+
+    author_user_id INT NOT NULL,
+    FOREIGN KEY (author_user_id) REFERENCES eventi.users(id)
+);
